95285a7c8b2a29fb4c8cc0970895c5941f346442
Vinícius <viniciusalmeidafreitas@gmail.com>
2022-07-26 01:44:18 -0300

    Implementing total cart price

--- src/components/CartItem/index.js
+++ src/components/CartItem/index.js
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 import React, { useContext } from 'react';
-import { Stack } from 'react-bootstrap';
+import { Button, Stack } from 'react-bootstrap';
 import { ShoppingCartContext } from '../../contexts/ShoppingCartContext';
 import products from '../../data/products.json';
+import { formatCurrency } from '../../utilities/formatCurrency';
 
 export function CartItem({ id, quantity }) {
   const { removeFromCart } = useContext(ShoppingCartContext);
@@ -14,7 +15,35 @@ export function CartItem({ id, quantity }) {
 
   return (
     <Stack direction="horizontal" gap="2" className="d-flex align-items-center">
-      <img src={item.imgUrl} alt={`${item.name}`} style={{ width: '125px', heigth: '75px', objectFit: 'cover' }} />
+      <img src={item.imgUrl} alt={`${item.name}`} style={{ width: '125px', height: '75px', objectFit: 'cover' }} />
+      <div className="me-auto">
+        {item.name}
+        {' '}
+        { quantity > 1
+        && (
+        <span className="text-muted" style={{ fontSize: '.65rem' }}>
+          {quantity}
+          x
+        </span>
+        )}
+        <div>
+          <span className="text-muted" style={{ fontSize: '.70rem' }}>
+            {formatCurrency(item.price)}
+          </span>
+        </div>
+      </div>
+      <div>
+        <span style={{ fontSize: '1rem' }}>{formatCurrency(item.price * quantity)}</span>
+      </div>
+      <Button
+        variant="outline-danger"
+        size="sm"
+        onClick={() => {
+          removeFromCart(item.id);
+        }}
+      >
+        &times;
+      </Button>
     </Stack>
   );
 }
--- src/components/ShoppingCart/index.js
+++ src/components/ShoppingCart/index.js
@@ -2,6 +2,8 @@ import React, { useContext } from 'react';
 import { Offcanvas, Stack } from 'react-bootstrap';
 import { ShoppingCartContext } from '../../contexts/ShoppingCartContext';
 import { CartItem } from '../CartItem';
+import products from '../../data/products.json';
+import { formatCurrency } from '../../utilities/formatCurrency';
 
 export function ShoppingCart() {
   const {
@@ -16,8 +18,20 @@ export function ShoppingCart() {
         <Stack gap="3">
           {cartQuantity === 0 ? <span>Nenhum produto no carrinho</span>
             : cartItems.map((item) => (
-              <CartItem key={item.id} id={item.id} />
+              <CartItem key={item.id} {...item} />
             ))}
+          <div className="ms-auto fw-bold fs-5">
+            Total:
+            {' '}
+            {
+              formatCurrency(cartItems.reduce((total, cartItem) => {
+                const item = products.find((i) => i.id === cartItem.id);
+                return (
+                  total + (item?.price || 0) * cartItem.quantity
+                );
+              }, 0))
+            }
+          </div>
         </Stack>
       </Offcanvas.Body>
     </Offcanvas>
